26f8bc07752eb90532218a45e6e433346389c9c3
kim hyoung joon <110275678+hyoungj00n@users.noreply.github.com>
2023-11-06 15:00:03 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,10 @@
+def solution(brown, yellow):
+    answer = []
+    total = brown+yellow
+    for b in range(1,total+1):
+        if total % b == 0:
+            a = total / b
+            if 2*a+2*b-4 == brown:
+                answer = [b,a]
+            
+    return answer
