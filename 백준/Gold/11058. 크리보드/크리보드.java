2c8c98381e8a11decdc16019f959ecc4289a2f45
kim hyoung joon <110275678+hyoungj00n@users.noreply.github.com>
2023-11-28 20:55:55 +0900

    [Gold V] Title: 크리보드, Time: 128 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Gold/11058. 크리보드/크리보드.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main{
+
+    static int N;
+    static long[] dp;
+
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        dp = new long[N+1];
+        for (int i = 1; i <= N; i++){
+
+            if(i <= 6){
+                dp[i] = i;
+                continue;
+            }
+
+            for (int j = 2; j <= 5; j++){
+                dp[i] = Math.max(dp[i], dp[i-(j+1)] *j);
+            }
+
+
+
+
+
+        }
+        System.out.println(dp[N]);
+    }
+
+
+}
