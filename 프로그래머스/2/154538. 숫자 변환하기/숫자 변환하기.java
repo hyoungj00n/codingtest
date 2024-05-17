da22400d10d4348d1ac070dc6a4904f5bf279e40
kim hyoung joon <110275678+hyoungj00n@users.noreply.github.com>
2024-05-17 21:54:20 +0900

    [level 2] Title: 숫자 변환하기, Time: 16.57 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int solution(int x, int y, int n) {
+        int answer = 0;
+        int[] dp = new int[y+1];
+        int count = 0;
+        
+        for(int i = x; i <= y; i++){
+            
+            if(i != x && dp[i] == 0){
+                dp[i] = -1;
+                continue;
+            }
+            
+            if(i * 3 <= y){
+                if(dp[i*3] == 0){
+                    dp[i*3] = dp[i] +1;
+                }else{
+                    dp[i*3] = Math.min(dp[i*3], dp[i]+1);
+                }
+
+            }
+            if(i * 2 <= y){
+                if(dp[i*2] == 0){
+                    dp[i*2] = dp[i] +1;
+                }else{
+                    dp[i*2] = Math.min(dp[i*2], dp[i]+1);
+                }
+
+            }
+            if(i + n <= y){
+                if(dp[i+n] == 0){
+                    dp[i+n] = dp[i] + 1;
+                }else{
+                    dp[i+n] = Math.min(dp[i+n], dp[i]+1);
+                }
+            }
+            
+            
+            
+        }
+        return dp[y];
+    }
+}
