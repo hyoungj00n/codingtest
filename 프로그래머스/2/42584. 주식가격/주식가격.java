2b438db3c2b7c7d337f89908054e600cae48c2a5
kim hyoung joon <110275678+hyoungj00n@users.noreply.github.com>
2024-05-19 21:05:06 +0900

    [level 2] Title: 주식가격, Time: 26.31 ms, Memory: 62.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,20 +2,22 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        Stack<Integer> stack = new Stack<>();
         
+        Stack<Integer> stack = new Stack<>();
+      
         for(int i = 0; i < prices.length; i++){
+            
             while(!stack.isEmpty() && prices[i] < prices[stack.peek()]){
-                answer[stack.peek()] = i - stack.peek();
-                stack.pop();
+                answer[stack.peek()] = i-stack.pop();
+                
             }
+            
             stack.push(i);
         }
-        
         while(!stack.isEmpty()){
-            answer[stack.peek()] = prices.length - stack.peek()-1;
-            stack.pop();
+            answer[stack.peek()] = prices.length - stack.pop()-1;
         }
+        
         return answer;
     }
 }
