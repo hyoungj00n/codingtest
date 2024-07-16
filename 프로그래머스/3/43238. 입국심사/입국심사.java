9374d9cd921b39301fb9714d7314648ccbb2c88f
kim hyoung joon <110275678+hyoungj00n@users.noreply.github.com>
2024-07-16 22:57:05 +0900

    [level 3] Title: 입국심사, Time: 76.01 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = 0;
+        long timeMax = Arrays.stream(times).max().getAsInt();
+        
+        long left = 0;
+        long right = timeMax * n;
+        while(left <= right){
+            long mid = (left + right) / 2;
+            long sum = 0;
+            for(int i = 0; i < times.length; i++){
+                sum += mid / times[i];
+            }
+            
+            if(sum < n){
+                left = mid+1;
+            }else{
+                right = mid-1;
+                answer = mid;
+            }
+        }
+        return answer;
+    }
+}
