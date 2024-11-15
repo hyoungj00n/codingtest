d42a1d82e9afd1dfb9db662b44147c8d06ef70ce
kim hyoung joon <110275678+hyoungj00n@users.noreply.github.com>
2024-11-15 16:47:09 +0900

    [Gold IV] Title: 부분합, Time: 252 ms, Memory: 25544 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,42 +1,51 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException{
 
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        Scanner sc = new Scanner(System.in);
+        st = new StringTokenizer(br.readLine());
 
-        int N = sc.nextInt();
-        int S = sc.nextInt();
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[N+1];
+        String[] arr = br.readLine().split(" ");
+        int[] num = new int[N+1];
 
-        for (int i =0; i< N; i++){
-            arr[i] = sc.nextInt();
+        for(int i= 0; i < arr.length; i++){
+            num[i] = Integer.parseInt(arr[i]);
         }
 
-        int min = Integer.MAX_VALUE;
         int start = 0;
         int end = 0;
-        int total = 0;
+        int length = Integer.MAX_VALUE;
+        int sum = 0;
+
         while(start <= N && end <= N){
-            if (total >= S && min > end - start){
-                min = end - start;
+            if( sum >= S && length > end-start){
+                length = end - start;
             }
-            if(total < S) {
-                total += arr[end++];
-            } else {
-                total -= arr[start++];
+            if(sum < S){
+                sum += num[end++];
+            }else{
+                sum -= num[start++];
             }
+
         }
 
-        if (min == Integer.MAX_VALUE){
+        if (length == Integer.MAX_VALUE){
             System.out.println("0");
-        } else System.out.println(min);
-
+        } else System.out.println(length);
 
     }
+
+
 }
